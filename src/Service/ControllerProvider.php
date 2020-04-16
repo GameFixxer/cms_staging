d8a9a187319e54b4c97ab47851a985a8312fa420
= <=r.berndt@nexus-united.com>
2020-04-16 12:58:52 +0200

    Refactoring of Array

--- src/Service/ControllerProvider.php
+++ src/Service/ControllerProvider.php
@@ -11,6 +11,7 @@ use App\Controller\ListControll;
 class ControllerProvider
 {
     private array $list;// = array();
+
     public function __construct()
     {
         $this->createListOfController();
@@ -24,30 +25,30 @@ class ControllerProvider
                     $this->list[] = array([ListControll::class]['list']);
                     return $this->list;
             */
-        $this->list = array(
-            (0) => Array
-            (
-                ('class') => DetailControll::class,
-                ('code') => 'detail',
-            ),
+        $this->list = [
+            (0) =>
+                [
+                    'class' => DetailControll::class,
+                    'code' => 'detail',
+                ],
 
-            (1) => Array
-            (
-                ('class') => ErrorControll::class,
-                ('code') => 'error',
-            ),
+            (1) =>
+                [
+                    'class' => ErrorControll::class,
+                    'code' => 'error',
+                ],
 
-            (2) => Array
-            (
-                ('class') => HomeControll::class,
-                ('code') => 'home',
-            ),
-            (2) => Array
-            (
-                ('class') => ListControll::class,
-                ('code') => 'list',
-            )
-        );
+            (2) =>
+                [
+                    'class' => HomeControll::class,
+                    'code' => 'home',
+                ],
+            (2) =>
+                [
+                    'class' => ListControll::class,
+                    'code' => 'list',
+                ]
+        ];
     }
 
     public function returnList(): array
@@ -57,11 +58,10 @@ class ControllerProvider
 
     public function inArrayMultidimension(string $needle, string $column): bool
     {
-        if(in_array($needle, array_column($this->list, $column))) {
+        if (in_array($needle, array_column($this->list, $column))) {
             return true;
 
-        }
-        else {
+        } else {
             return false;
 
         }
@@ -74,6 +74,6 @@ class ControllerProvider
 
     public function returnObject(int $key)
     {
-        return $this->list[$key]('class');
+        return $this->list[$key]['class'];
     }
 }
