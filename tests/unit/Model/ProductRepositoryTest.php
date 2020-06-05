08abbd779e2a5c98ce513b3225d142981fa59d46
= <=r.berndt@nexus-united.com>
2020-06-05 16:48:07 +0200

    added unittest

---
+++ tests/unit/Model/ProductRepositoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Model\ProductRepository;
+
+class ProductRepositoryTest extends \Codeception\Test\Unit
+{
+    private ProductRepository $productrepository;
+
+    public function testGetProductList()
+    {
+        $db = new \App\Service\DatabaseManager();
+        $productrepository = new ProductRepository($db->connect());
+        $productrepository->getProductList();
+    }
+
+    public function testGetProduct()
+    {
+    }
+
+    public function testHasProduct()
+    {
+        $db = new \App\Service\DatabaseManager();
+        $productrepository = new ProductRepository($db->connect());
+        self::assertFalse($productrepository->hasProduct(0));
+    }
+
+    public function test__construct()
+    {
+    }
+}
---
+++ tests/unit/Model/UserRepositoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Model\UserRepository;
+
+class UserRepositoryTest extends \Codeception\Test\Unit
+{
+
+    public function testGetUser()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testHasUser()
+    {
+
+    }
+
+    public function testGetUserList()
+    {
+
+    }
+}
