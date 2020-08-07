17256f07d0e739a689aa9871a051ac05d5071667
= <=r.berndt@nexus-united.com>
2020-08-07 14:47:16 +0200

    order

--- src/Client/Order/Persistence/Mapper/OrderMapper.php
+++ src/Client/Order/Persistence/Mapper/OrderMapper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace App\Client\Order\Persistence\Mapper;
 
 use App\Client\Order\Persistence\Entity\Order;
+use App\Client\Product\Persistence\Mapper\ProductMapper;
+use App\Client\User\Persistence\Mapper\UserMapper;
 use App\Generated\Dto\OrderDataTransferObject;
 
 class OrderMapper implements OrderMapperInterface
@@ -11,7 +13,6 @@ class OrderMapper implements OrderMapperInterface
     public function map(Order $order): OrderDataTransferObject
     {
         $orderDataTransferObject = new   OrderDataTransferObject();
-
         $orderDataTransferObject->setOrderId($order->getOrderId());
         $orderDataTransferObject->setStatus($order->getStatus());
         $orderDataTransferObject->setUser($order->getUser());
--- src/Frontend/Order/Communication/OrderController.php
+++ src/Frontend/Order/Communication/OrderController.php
@@ -3,14 +3,76 @@ declare(strict_types=1);
 
 namespace App\Frontend\Order\Communication;
 
-
+use App\Client\Address\Business\AddressBusinessFacadeInterface;
+use App\Client\Order\Business\OrderBusinessFacadeInterface;
+use App\Client\Product\Business\ProductBusinessFacadeInterface;
+use App\Client\User\Business\UserBusinessFacadeInterface;
+use App\Component\View;
 use App\Frontend\BackendController;
+use App\Frontend\User\Communication\DashboardController;
+use App\Generated\Dto\OrderDataTransferObject;
+use App\Service\SessionUser;
 
 class OrderController implements BackendController
 {
+    public const ROUTE = 'order';
+    private SessionUser $userSession;
+    private View $view;
+    private UserBusinessFacadeInterface $userBusinessFacade;
+    private OrderBusinessFacadeInterface $businessFacade;
+    private OrderDataTransferObject $orderDataTransferObject;
+    private AddressBusinessFacadeInterface $addressBusinessFacade;
+    private ProductBusinessFacadeInterface $productBusinessFacade;
+
+    public function __construct(
+        View $view,
+        UserBusinessFacadeInterface $userBusinessFacade,
+        SessionUser $userSession,
+        OrderBusinessFacadeInterface $businessFacade,
+        AddressBusinessFacadeInterface $addressBusinessFacade,
+        ProductBusinessFacadeInterface $productBusinessFacade
+    ) {
+        $this->userSession = $userSession;
+        $this->view = $view;
+        $this->userBusinessFacade = $userBusinessFacade;
+        $this->businessFacade = $businessFacade;
+        $this->addressBusinessFacade = $addressBusinessFacade;
+        $this->productBusinessFacade = $productBusinessFacade;
+    }
 
     public function init(): void
     {
-        // TODO: Implement init() method.
+        if ($this->userSession->isLoggedIn() && !($_GET['page'] === 'logout')) {
+            $this->view->setRedirect(self::ROUTE, '&page=list', ['admin=true']);
+            $this->orderDataTransferObject->setUser($this->userBusinessFacade->get($this->userSession->getUser()));
+        }
+        $this->view->addTlpParam('login', 'LOGIN AREA');
+    }
+
+    public function action()
+    {
+    }
+
+    private function addShoppingCardItems()
+    {
+        $card = $this->userSession->getShoppingCard();
+        $sum = 0;
+        foreach ($card as $item) {
+            $sum = $sum + $this->productBusinessFacade->get($item)->getPrice();
+        }
+        $this->orderDataTransferObject->setSum($sum);
+        $this->orderDataTransferObject->setOrderedProducts(implode(',', $card));
+    }
+
+    private function addAddressToOrder(string $type, bool $primary)
+    {
+        $this->orderDataTransferObject->setAddress(
+            $this->addressBusinessFacade->get($this->orderDataTransferObject->getUser(), $type, $primary)
+        );
+    }
+
+    private function pushOrder()
+    {
+        $this->businessFacade->save($this->orderDataTransferObject);
     }
 }
