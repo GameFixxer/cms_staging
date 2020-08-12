56f0ff5b32ab46b575ca16ae27c5a20f181319d1
= <=r.berndt@nexus-united.com>
2020-08-12 15:43:56 +0200

    issues

---
+++ src/Client/ShoppingCard/Business/ShoppingCardBusinessFacadeInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Client\ShoppingCard\Business;
+
+use App\Generated\ShoppingCardDataProvider;
+
+interface ShoppingCardBusinessFacadeInterface
+{
+    public function get(int $id): ?ShoppingCardDataProvider;
+
+    /**
+     * @param ShoppingCardDataProvider $shoppingCardDataProvider
+     * @return ShoppingCardDataProvider
+     */
+    public function save(ShoppingCardDataProvider $shoppingCardDataProvider): ShoppingCardDataProvider;
+
+    public function delete(ShoppingCardDataProvider $shoppingCardDataProvider);
+}
