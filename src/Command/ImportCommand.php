f114a575e9407542c4a570b8f4f9dbd1c9cabf13
Kai <k.urban@nexus-united.com>
2021-10-01 14:50:25 +0200

    bin/console now works

---
+++ src/Command/ImportCommand.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportCommand extends Command
+{
+    private const COMMAND = 'data:import';
+    private const DESCRIPTION = 'Import all necessary data';
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName(self::COMMAND)
+            ->setDescription(self::DESCRIPTION);
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // TODO add logic
+        return 0;
+    }
+}
