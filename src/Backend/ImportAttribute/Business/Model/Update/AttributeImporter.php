dd8bd9cda3b35557db4294d812f412156c6ce880
Kai <k.urban@nexus-united.com>
2022-09-28 08:36:18 +0200

    refactored AttributeImporter class

--- src/Backend/ImportAttribute/Business/Model/Update/AttributeImporter.php
+++ src/Backend/ImportAttribute/Business/Model/Update/AttributeImporter.php
@@ -3,28 +3,42 @@ declare(strict_types=1);
 
 namespace App\Backend\ImportAttribute\Business\Model\Update;
 
+use Exception;
 use App\Backend\ImportProduct\Business\Model\ActionProvider;
 use App\Generated\Dto\CsvAttributeDataTransferObject;
 
 class AttributeImporter implements AttributeImporterInterface
 {
-    private array $importArrayList;
+    /**
+     * @var \App\Backend\ImportProduct\Business\Model\ActionProvider
+     */
+    private ActionProvider $filterProvider;
 
+    /**
+     * @param \App\Backend\ImportProduct\Business\Model\ActionProvider $filterProvider
+     */
     public function __construct(ActionProvider $filterProvider)
     {
-        $this->importArrayList = $filterProvider->getAttributeActionList();
+        $this->filterProvider = $filterProvider;
     }
 
-    public function performUpdateActions(CsvAttributeDataTransferObject $csvDTO):void
+    /**
+     * @param \App\Generated\Dto\CsvAttributeDataTransferObject $csvDTO
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function performUpdateActions(CsvAttributeDataTransferObject $csvDTO): void
     {
-        foreach ($this->importArrayList as $action) {
+        foreach ($this->filterProvider->getAttributeActionList() as $action) {
             if ($action === null) {
-                throw new \Exception('Filter or Updatefunction broken (action = null)', 1);
+                throw new Exception('Filter or Updatefunction broken (action = null)', 1);
             }
+
             try {
                 $action->update($csvDTO);
-            } catch (\Exception $e) {
-                throw new \Exception(get_class($action) . ' crashed', 1);
+            } catch (Exception $e) {
+                throw new Exception(get_class($action) . ' crashed', 1);
             }
         }
     }
