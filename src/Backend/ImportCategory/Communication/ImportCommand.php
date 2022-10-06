2a87849b286939a291cf60f0ed90c9ea5c0b2832
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2022-10-06 11:45:07 +0200

    fix doc block

--- src/Backend/ImportCategory/Communication/ImportCommand.php
+++ src/Backend/ImportCategory/Communication/ImportCommand.php
@@ -37,7 +37,9 @@ class ImportCommand extends Command
     /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
-     * @return mixed
+     *
+     * @throws \Exception
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
